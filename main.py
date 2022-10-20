977ce64c139431aef6ccbb5d8e50bf629e29f87a
Evgenii Zhmero-Gaidenko <106560016+jmuriki@users.noreply.github.com>
2022-10-20 16:57:28 +0300

    Add some improvements
    - add parse_book_page
    - add get_genres
    - transform get_title to split_title
    - offload main

--- main.py
+++ main.py
@@ -21,9 +21,18 @@ def get_soup(url, id):
     return BeautifulSoup(response.text, "lxml")
 
 
-def get_title(soup):
+def split_title(soup):
     title_tag = soup.find("body").find("table", class_="tabs").find("h1")
-    return title_tag.text.split("::")[0].strip()
+    return title_tag.text.split("::")
+
+
+def get_genres(soup):
+    genres = []
+    genres_tag = soup.find_all("span", class_="d_book")
+    for teg in genres_tag:
+        genre = teg.find("a").text
+        genres.append(genre)
+    return "\n".join(genres)
 
 
 def get_comments_texts(soup):
@@ -35,17 +44,27 @@ def get_comments_texts(soup):
     return "\n".join(texts)
 
 
-def compile_commets_guide(guide, soup, bookname):
-    guide.append(bookname)
+def parse_book_page(soup):
+    book_parsed = {}
+    book_parsed["Заголовок"] = split_title(soup)[0].strip()
+    book_parsed["Автор"] = split_title(soup)[-1].strip()
+    book_parsed["Жанр"] = get_genres(soup)
+    book_parsed["Отзывы"] = get_comments_texts(soup)
+    book_parsed["Обложка"] = soup.find("body").find("div", class_="bookimage").find("img")["src"]
+    return book_parsed
+
+
+def compile_commets_guide(book_parsed):
+    guide = []
+    guide.append(book_parsed["Заголовок"])
+    guide.append(book_parsed["Автор"])
+    if book_parsed["Отзывы"]:
+        guide.append(book_parsed["Отзывы"])
     guide.append("\n")
-    texts = get_comments_texts(soup)
-    if texts:
-        guide.append(texts)
-        guide.append("\n")
-    return guide
+    return "\n".join(guide)
 
 
-def download_txt(url, filename, folder='books/'):
+def download_txt(url, filename, folder="books/"):
     response = requests.get(url)
     response.raise_for_status()
     Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
@@ -55,7 +74,7 @@ def download_txt(url, filename, folder='books/'):
     return path
 
 
-def download_image(url, filename, folder='images/'):
+def download_image(url, filename, folder="images/"):
     response = requests.get(url)
     response.raise_for_status()
     Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
@@ -65,7 +84,7 @@ def download_image(url, filename, folder='images/'):
     return path
 
 
-def save_comments(comments_guide, filename='Гид по отзывам.txt', folder='books/'):
+def save_comments(comments_guide, filename="Гид по отзывам.txt", folder="books/"):
     Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
     path = Path(f"./{folder}{filename}")
     with open(path, "w") as file:
@@ -80,17 +99,13 @@ def main():
         try:
             check_for_redirect(txt_url)
             soup = get_soup(url, id)
-            title = get_title(soup)
-            print("Заголовок: ", title)
-            filename = sanitize_filename(f"{id}. {title}.txt")
+            book_parsed = parse_book_page(soup)
+            filename = sanitize_filename(f"{id}. {book_parsed['Заголовок']}.txt")
             download_txt(url, filename)
-            comments_guide = compile_commets_guide(comments_guide, soup, filename)
-            genre = soup.find("span", class_="d_book").find("a")
-            print(genre.text, "\n")
-            rel_link = soup.find("body").find("div", class_="bookimage").find("img")["src"]
-            pic_url = urljoin(url, rel_link)            
-            pic_name = os.path.split(rel_link)[-1]
+            pic_url = urljoin(url, book_parsed["Обложка"])
+            pic_name = os.path.split(book_parsed["Обложка"])[-1]
             download_image(pic_url, pic_name)
+            comments_guide.append(compile_commets_guide(book_parsed))
         except:
             continue
     save_comments(comments_guide)

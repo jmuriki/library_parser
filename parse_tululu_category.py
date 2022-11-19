26e8cf559673efc81480e7371e9fc1998551e3ee
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-11-19 15:46:02 +0300

    Add more argparse arguments

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -11,13 +11,67 @@ from pathvalidate import sanitize_filename
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description="""
-        Опциональные аргументы.
-        Введите номер первой и номер последней страницы (последняя не будет скачана).
-        По умолчанию будут скачаны книги со всех доступных страниц.
-    """)
-    parser.add_argument("-s", "--start_page", default=1, type=int)
-    parser.add_argument("-e", "--end_page", default=1000000, type=int)
+    parser = argparse.ArgumentParser(
+        description="""Все представленные аргументы являются опциональными.
+        По умолчанию будут скачаны все книги и картинки со всех доступных страниц
+        в заранее определённые папки в корневом каталоге проекта."""
+    )
+    parser.add_argument(
+        "-g",
+        "--genre",
+        default=55,
+        type=int,
+        help="""Введите номер жанра.
+        По умолчанию будет указан номер 55, что соответствует жанру
+        "Научная фантастика"."""
+    )
+    parser.add_argument(
+        "-s",
+        "--start_page",
+        default=1,
+        type=int,
+        help="""Введите номер начальной страницы.
+                Если не вводить номер конечной страницы,
+                будут скачаны все доступные страницы
+                с начальной включительно."""
+    )
+    parser.add_argument(
+        "-e",
+        "--end_page",
+        default=1000000,
+        type=int,
+        help="""Введите номер конечной страницы.
+                Если не вводить номер начальной страницы,
+                будут скачаны все доступные страницы с первой по конечную
+                (без включения конечной)."""
+    )
+    parser.add_argument(
+        "-f",
+        "--dest_folder",
+        default=".",
+        help="""Введите путь к каталогу с результатами парсинга:
+                картинкам, книгам, JSON."""
+    )
+    parser.add_argument(
+        "-i",
+        "--skip_imgs",
+        action="store_true",
+        help="""По умолчанию картинки будут скачаны.
+                Для отмены укажите при запуске аргумент без значения."""
+    )
+    parser.add_argument(
+        "-t",
+        "--skip_txt",
+        action="store_true",
+        help="""По умолчанию тексты книг будут скачаны.
+                Для отмены укажите при запуске аргумент без значения."""
+    )
+    parser.add_argument(
+        "-j",
+        "--json_path",
+        default=None,
+        help="Введите путь к *.json файлу с результатами."
+    )
     return parser
 
 
@@ -77,39 +131,46 @@ def get_books_rel_paths(url, genre, start_page, end_page):
     return paths
 
 
-def download_txt(txt_url, params, txt_name, folder="books/"):
+def download_txt(txt_url, params, txt_name, dest_folder, folder="books"):
     response = requests.get(txt_url, params)
     response.raise_for_status()
     check_for_redirect(response)
-    Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
-    path = Path(f"./{folder}{txt_name}")
+    Path(f"{dest_folder}/{folder}").mkdir(parents=True, exist_ok=True)
+    path = Path(f"{dest_folder}/{folder}/{txt_name}")
     with open(path, "w") as file:
         file.write(response.text)
     return str(path)
 
 
-def download_image(pic_url, pic_name, folder="images/"):
+def download_image(pic_url, pic_name, dest_folder, folder="images"):
     response = requests.get(pic_url)
     response.raise_for_status()
     check_for_redirect(response)
-    Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
-    path = Path(f"./{folder}{pic_name}")
+    Path(f"{dest_folder}/{folder}").mkdir(parents=True, exist_ok=True)
+    path = Path(f"{dest_folder}/{folder}/{pic_name}")
     with open(path, "wb") as file:
         file.write(response.content)
     return str(path)
 
 
-def save_as_json(books_info):
-    with open(Path("./books_info.json"), "w", encoding="utf8") as file:
+def save_as_json(books_info, dest_folder, json_path, filename="books_info"):
+    folder = json_path if json_path else dest_folder
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    path = Path(f"{folder}/{filename}.json")
+    with open(path, "w", encoding="utf8") as file:
         json.dump(books_info, file, ensure_ascii=False)
 
 
 def main():
     arguments = create_parser().parse_args()
+    genre = arguments.genre
     start_page = arguments.start_page
     end_page = arguments.end_page
+    dest_folder = arguments.dest_folder
+    skip_imgs = arguments.skip_imgs
+    skip_txt = arguments.skip_txt
+    json_path = arguments.json_path
     url = "https://tululu.org/"
-    genre = 55
     books_rel_paths = get_books_rel_paths(
         url,
         genre,
@@ -127,15 +188,17 @@ def main():
         try:
             soup = get_soup(book_url)
             parsed_page = parse_book_page(soup)
-            txt_name = sanitize_filename(f"{parsed_page['title']}.txt")
-            book_path = download_txt(txt_url, params, txt_name)
-            pic_url = urljoin(book_url, parsed_page['pic_rel_path'])
-            pic_name = parsed_page['pic_name']
-            pic_path = download_image(pic_url, pic_name)
-            parsed_page["book_path"] = book_path
-            parsed_page["img_src"] = pic_path
-            parsed_page.pop("pic_rel_path")
-            parsed_page.pop("pic_name")
+            if not skip_txt:
+                txt_name = sanitize_filename(f"{parsed_page['title']}.txt")
+                book_path = download_txt(txt_url, params, txt_name, dest_folder)
+                parsed_page["book_path"] = book_path
+            if not skip_imgs:
+                pic_url = urljoin(book_url, parsed_page['pic_rel_path'])
+                pic_name = parsed_page['pic_name']
+                pic_path = download_image(pic_url, pic_name, dest_folder)
+                parsed_page["img_src"] = pic_path
+                parsed_page.pop("pic_rel_path")
+                parsed_page.pop("pic_name")
             books_info.append(parsed_page)
         except requests.exceptions.HTTPError as error:
             print(error)
@@ -146,7 +209,7 @@ def main():
         except Exception as error:
             print(error)
             continue
-    save_as_json(books_info)
+    save_as_json(books_info, dest_folder, json_path)
 
 
 if __name__ == "__main__":

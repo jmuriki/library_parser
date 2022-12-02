da983beb86bca4835f140fb36f4bc32882e0ef56
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-12-03 01:45:31 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,35 +1,41 @@
 import json
 
+from pathlib import Path
 from livereload import Server
 from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def on_reload():
+	pages_folder = "pages"
+	Path(f"./{pages_folder}").mkdir(parents=True, exist_ok=True)
 	with open("books_descriptions.json", "r") as file:
 		books_descriptions = json.load(file)
 	for description in books_descriptions:
 		description["book_path"] = description["book_path"].replace("\\", "/")
 		description["img_src"] = description["img_src"].replace("\\", "/")
-	books_descriptions = [chunk for chunk in chunked(books_descriptions, 2)]
+	paged_descriptions = [chunk for chunk in chunked(books_descriptions, 20)]
+	packed_descriptions = [[chunk for chunk in chunked(page, 2)] for page in paged_descriptions]
 	env = Environment(
-	    loader=FileSystemLoader('.'),
-	    autoescape=select_autoescape(['html', 'xml'])
+	    loader=FileSystemLoader("."),
+	    autoescape=select_autoescape(["html", "xml"])
 	)
-	template = env.get_template('template.html')
-	rendered_page = template.render(
-	    books_descriptions = books_descriptions,
-	)
-	with open('index.html', 'w', encoding="utf8") as file:
-	    file.write(rendered_page)
+	template = env.get_template("template.html")
+	for number, page in enumerate(packed_descriptions):
+		rendered_page = template.render(
+		    books_descriptions = page,
+		)
+		with open(f"./{pages_folder}/index{number + 1}.html", "w", encoding="utf8") as file:
+		    file.write(rendered_page)
 
 
 def main():
 	on_reload()
 	server = Server()
-	server.watch('template.html', on_reload)
-	server.serve(root='.')
+	server.watch("template.html", on_reload)
+	server.serve(root=".")
 
 
 if __name__ == "__main__":
     main()
+ 

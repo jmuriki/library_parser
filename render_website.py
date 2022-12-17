65face5c2e04d1ea65ece3dbb0bbbdbe5acb22d7
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-12-17 13:58:53 +0300

    Replace abbreviations in variables names

--- render_website.py
+++ render_website.py
@@ -6,35 +6,39 @@ from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def get_descs():
+def get_descriptions():
     with open("books_descriptions.json", "r") as file:
-        descs = json.load(file)
-    for desc in descs:
-        desc["book_path"] = desc["book_path"].replace("\\", "/")
-        desc["img_src"] = desc["img_src"].replace("\\", "/")
-    return descs
+        descriptions = json.load(file)
+    for description in descriptions:
+        description["book_path"] = description["book_path"].replace("\\", "/")
+        description["img_src"] = description["img_src"].replace("\\", "/")
+    return descriptions
 
 
-def arrange_descs(descs):
-    descs_per_line = 2
+def arrange_descriptions(descriptions):
+    descriptions_per_line = 2
     lines_per_page = 10
-    lined_descs = [chunk for chunk in chunked(descs, descs_per_line)]
-    paged_descs = [chunk for chunk in chunked(lined_descs, lines_per_page)]
-    return paged_descs
+    lined_descriptions = [chunk for chunk in chunked(
+            descriptions, descriptions_per_line
+        )]
+    paged_descriptions = [chunk for chunk in chunked(
+            lined_descriptions, lines_per_page
+        )]
+    return paged_descriptions
 
 
 def on_reload():
-    descs = get_descs()
+    descriptions = get_descriptions()
     pages_folder_name = "pages"
     Path(f"./{pages_folder_name}").mkdir(parents=True, exist_ok=True)
-    paged_descs = arrange_descs(descs)
+    paged_descriptions = arrange_descriptions(descriptions)
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html", "xml"])
     )
     template = env.get_template("template.html")
-    pages_nums = [num for num, _ in enumerate(paged_descs, 1)]
-    for number, page in enumerate(paged_descs, 1):
+    pages_nums = [num for num, _ in enumerate(paged_descriptions, 1)]
+    for number, page in enumerate(paged_descriptions, 1):
         rendered_page = template.render(
             pages_nums=pages_nums,
             number=number,
